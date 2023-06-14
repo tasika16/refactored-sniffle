e97022801af7ebedece20c35234e1856d948cf7c
tasika16 <tasika696@gmail.com>
2023-06-14 13:58:49 +0200

    Init

--- src/main/java/com/tasnadi/photosclone/PhotosCloneApplication.java
+++ src/main/java/com/tasnadi/photosclone/PhotosCloneApplication.java
@@ -1,12 +1,7 @@
 package com.tasnadi.photosclone;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 public class PhotosCloneApplication {
